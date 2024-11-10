80a2f00244fc586e3847e577bbf4ccf6355e8117
Martin Deyanov <martopvp123@gmail.com>
2024-11-10 02:02:59 +0100

    Added "about"blank" page, refactored layout into a class that handles different font size, styles and html tags

--- browser.py
+++ browser.py
@@ -5,12 +5,17 @@ import time
 import gzip
 import tkinter
 import sys
-from urllib.parse import urljoin
+import tkinter.font
+from urllib.parse import urljoin, urlparse
 
 class URL:
     cache = {}
 
     def __init__(self, url):
+        if not is_valid_url(url):
+            self.scheme = "about"
+            return
+        
         # Data scheme for inline HTML support
         if url[:4] == "data":
             self.scheme, self.html_content = url.split(":", 1)
@@ -47,6 +52,10 @@ class URL:
         self.path = "/" + url
     
     def request(self, max_redirects=5):
+        # Check if the url is malformed
+        if self.scheme == "about":
+            return "about:blank"
+        
         # Check if the URL is cached and still valid
         if self.scheme in ["http", "https"]:
             cache_key = f"{self.scheme}://{self.host}{self.path}"
@@ -164,6 +173,13 @@ class URL:
             s.close()
             return None
 
+def is_valid_url(url):
+        try:
+            result = urlparse(url)
+            return all([result.scheme, result.netloc]) or url == "about:blank"
+        except ValueError:
+            return False
+
 def add_headers(request, headers: dict):
     assert "Host" in request
 
@@ -174,6 +190,15 @@ def add_headers(request, headers: dict):
 
 
 
+
+class Text:
+    def __init__(self, text):
+        self.text = text
+
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 SCROLL_STEP = 100
@@ -183,7 +208,7 @@ class Browser:
     
     def __init__(self):
         self.display_list = []
-        self.text = ""
+        self.tokens = []
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
             self.window,
@@ -213,7 +238,7 @@ class Browser:
     def on_resize(self, e):
         global WIDTH, HEIGHT
         WIDTH, HEIGHT = e.width, e.height
-        self.display_list = layout(self.text)
+        self.display_list = Layout(self.tokens).display_list
         self.draw()
 
     def on_mouse_scroll(self, e):
@@ -230,7 +255,7 @@ class Browser:
 
     def scrolldown(self, e):
         # Get last display list element in order to get the position at the end of the page
-        _, y, _ = self.display_list[-1]
+        _, y, _, _ = self.display_list[-1]
         if y - self.scroll > HEIGHT - VSTEP: # Prevents scrolling beyond the last display list element
             self.scroll += SCROLL_STEP
             self.draw()
@@ -242,7 +267,7 @@ class Browser:
 
     def draw_scrollbar(self):
         # Get last element of display list to get the content height
-        _, y, _ = self.display_list[-1]
+        _, y, _, _ = self.display_list[-1]
 
         # If the whole content fits onscreen
         if y < HEIGHT:
@@ -263,76 +288,169 @@ class Browser:
     def draw(self):
         self.canvas.delete("all")
         self.draw_scrollbar()
-        for x,y,c in self.display_list:
+        for x,y,c,f in self.display_list:
             if y > self.scroll + HEIGHT: continue   # skip items below the visible area
             if y + VSTEP < self.scroll: continue    # skip items above the visible area
-            self.canvas.create_text(x, y-self.scroll, text=c)
+            self.canvas.create_text(x, y-self.scroll, text=c, anchor='nw', font=f)
     
     def load(self, url):
         body = url.request()
-        self.text = lex(body)
-        self.display_list = layout(self.text)
+        if body == "about:blank":
+            self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT, bg="white")
+            self.canvas.pack(fill=tkinter.BOTH, expand=True)
+        self.tokens = lex(body)
+        self.display_list = Layout(self.tokens, WIDTH).display_list
         self.draw()
 
-def layout(text):
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        if  c == "\n":   # New line, move the cursor down
-            cursor_x = HSTEP
-            cursor_y += VSTEP + 2
-        else:
-            display_list.append((cursor_x, cursor_y, c))
-            cursor_x += HSTEP
+FONTS = {}
 
-        if cursor_x >= WIDTH - HSTEP:
-            cursor_y += VSTEP
-            cursor_x = HSTEP
-    return display_list
+class Layout:
+    def __init__(self, tokens, canvas_width):
+        self.display_list = []
+        self.line = []
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.canvas_width = canvas_width
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+        self.center_next_text = False # Flag for centering text
+
+        for tok in tokens:
+            self.token(tok)
+        
+        self.flush()
+    
+    def token(self, tok):
+        if isinstance(tok, Text):
+            if self.center_next_text:
+                self.center_text(tok.text)     # Center this token's text
+                self.center_next_text = False  # Reset the flag
+            else:
+                for word in tok.text.split():
+                    self.word(word)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
+        elif tok.tag == "h1 class=\"title\"":
+            self.flush() # End the current line before centering
+            self.center_next_text = True  # Set flag to center the next text token
+        elif tok.tag == "/h1":
+            self.flush()
+            self.cursor_x = HSTEP
+    
+    def word(self, word):
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+
+        if  word == "\n":   # New line, move the cursor down
+            self.cursor_x = HSTEP
+            self.cursor_y += VSTEP + 2
+        else:
+            self.line.append((self.cursor_x, word, font))
+            self.cursor_x += w + font.measure(" ")
+    
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+
+        baseline = self.cursor_y + 1.25 * max_ascent
+
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+
+        self.cursor_x = HSTEP
+        self.line = []
+
+    def center_text(self, text):
+        font = get_font(self.size, self.weight, self.style)
+        text_width = font.measure(text)
+        self.cursor_x = (self.canvas_width - text_width) // 2
+        self.line.append((self.cursor_x, text, font))
+        self.flush()  # Finish the centered text line
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
 
 def lex(body):
-    text = ""
+    out = []
+    buffer = ""
     in_tag = False
 
     i = 0
-    skip_entity = 4
     while i < len(body):
         if body[i] == "<":
             in_tag = True
+            if buffer: out.append(Text(buffer))
+            buffer = ""
             i += 1
         elif body[i] == ">":
             in_tag = False
+            out.append(Tag(buffer))
+            buffer = ""
             i += 1
         elif body[i] == "&" and not in_tag:  # Check if the next sequence is an entity (&lt; or &gt;)
             if is_entity(body[i:i+4]):
                 if body[i+1] =="l":
-                    text += "<"
+                    buffer += "<"
                 else:
-                    text += ">"
-                i += skip_entity
+                    buffer += ">"
+                i += 4   # Skip entity by incrementing index
             else:
-                text += "&"
+                buffer += "&"
                 i += 1
-        elif not in_tag:
-            text += body[i]
+        else:
+            buffer += body[i]
             i += 1
-        elif in_tag:
-            i += 1
+    if not in_tag and buffer:
+        out.append(Text(buffer))
+        i += 1
     
-    return text
+    return out
 
 def is_entity(text):
     return text == "&lt;" or text == "&gt;"
 
-if __name__ == "__main__":
-    import sys
-    Browser().load(URL(sys.argv[1]))
-    tkinter.mainloop()
-
 # if __name__ == "__main__":
 #     import sys
-#     Browser().load(URL("https://browser.engineering/examples/xiyouji.html"))
+#     Browser().load(URL(sys.argv[1]))
 #     tkinter.mainloop()
+
+if __name__ == "__main__":
+    import sys
+    Browser().load(URL("https://browser.engineering/text.html"))
+    tkinter.mainloop()
 
 # Test URL's
 
@@ -342,6 +460,7 @@ if __name__ == "__main__":
 # file://D:/Martin/Projects/Browser/example.txt
 # view-source:http://example.org/
 # http://browser.engineering/redirect
+# https://browser.engineering/examples/example3-sizes.html
 
 # Journey to the West
 # https://browser.engineering/examples/xiyouji.html
